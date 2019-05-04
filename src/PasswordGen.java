059071dffc94a9ff2255c6f3487d02955fa5e926
Jack Maguire <jackmaguire1234@gmail.com>
2019-05-04 15:42:55 +0100

    New Method - More Accurate Based off of C# Version

--- src/PasswordGen.java
+++ src/PasswordGen.java
@@ -1,66 +1,115 @@
+import java.util.ArrayList;
 import java.util.Random;
 
 public class PasswordGen {
 
-    public static String getPassword (boolean numbersBool, boolean lettersBool, boolean specCharsB, char[] lettersArr, char[] specChars, int numbersN, int lettersN, int specCharsN)
+//    public static String getPassword (boolean numbersBool, boolean lettersBool, boolean specCharsB, char[] lettersArr, char[] specChars, int numbersN, int lettersN, int specCharsN)
+//    {
+//        int SCSoFar = 0;
+//        int nSoFar = 0;
+//        int lSoFar = 0;
+//
+//        String temp = "";
+//
+//        Random rnd = new Random();
+//
+//        int totSc;
+//        int totN;
+//        int totL;
+//
+//        if(!specCharsB)
+//            totSc = 0;
+//        else
+//            totSc = specCharsN;
+//
+//        if(!numbersBool)
+//            totN = 0;
+//        else
+//            totN = numbersN;
+//
+//        if(!lettersBool)
+//            totL = 0;
+//        else
+//            totL = lettersN;
+//
+//        for (int i = 0; i < totSc + totN + totL; i++)
+//        {
+//            double d = rnd.nextDouble();
+//
+//            if(d < 1 / 3.0 && SCSoFar != totSc)
+//            {
+//                if(specCharsB)
+//                {
+//                    char ch = specChars[rnd.nextInt(specChars.length)];
+//                    temp += ch;
+//                    SCSoFar++;
+//                }
+//            }
+//
+//            else if(d < 2 / 3.0 && lSoFar != totL)
+//            {
+//                if(lettersBool) {
+//                    char ch = lettersArr[rnd.nextInt(lettersArr.length)];
+//                    temp += ch;
+//                    SCSoFar++;
+//                }
+//            }
+//
+//            else if(nSoFar != totN)
+//            {
+//                if(numbersBool) {
+//                    int j = rnd.nextInt(10);
+//                    temp += j;
+//                    nSoFar++;
+//                }
+//            }
+//        }
+//
+//        return temp;
+//    }
+
+    public static String getPassword (boolean numbersBool, boolean lettersBool, boolean specCharsBool, char[] lettersArr, char[] specChars, int numbersN, int lettersN, int specCharsN)
     {
-        int SCSoFar = 0;
-        int nSoFar = 0;
-        int lSoFar = 0;
-
-        String temp = "";
-
         Random rnd = new Random();
 
-        int totSc;
-        int totN;
-        int totL;
-        if(!specCharsB)
-            totSc = 0;
-        else
-            totSc = specCharsN;
-        if(!numbersBool)
-            totN = 0;
-        else
-            totN = numbersN;
-        if(!lettersBool)
-            totL = 0;
-        else
-            totL = lettersN;
+        ArrayList<String> endList = new ArrayList<>();
 
-        for (int i = 0; i < totSc + totN + totL; i++)
+        if(numbersBool)
         {
-            double d = rnd.nextDouble();
-
-            if(d < 1 / 3.0 && SCSoFar != totSc)
-            {
-                if(specCharsB)
-                {
-                    char ch = specChars[rnd.nextInt(specChars.length)];
-                    temp += ch;
-                    SCSoFar++;
-                }
-            }
-
-            else if(d < 2 / 3.0 && lSoFar != totL)
-            {
-                if(lettersBool) {
-                    char ch = lettersArr[rnd.nextInt(lettersArr.length)];
-                    temp += ch;
-                    SCSoFar++;
-                }
-            }
-
-            else if(nSoFar != totN)
-            {
-                if(numbersBool) {
-                    int j = rnd.nextInt(10);
-                    temp += j;
-                    nSoFar++;
-                }
+            int bigN = (int) Math.pow(10, numbersN) - 1;
+            int rn = rnd.nextInt(bigN);
+            endList.add(rn + "");
+        }
+        if(lettersBool)
+        {
+            for (int i = 0; i < lettersN; i++) {
+                int rndN = rnd.nextInt(lettersArr.length);
+                char c = lettersArr[rndN];
+                endList.add(c + "");
             }
         }
+        if(specCharsBool)
+        {
+            for (int i = 0; i < specCharsN; i++) {
+                int rndN = rnd.nextInt(lettersArr.length);
+                char c = specChars[rndN];
+                endList.add(c + "");
+            }
+        }
+        
+        int size = endList.size();
 
-        return temp;
+        String str = "";
+
+        for (int i = 0; i < size; i++)
+        {
+            int rndN = rnd.nextInt(endList.size());
+            str += endList.get(rndN);
+            endList.remove(rndN);
+        }
+
+        return str;
+
     }
+
 }
